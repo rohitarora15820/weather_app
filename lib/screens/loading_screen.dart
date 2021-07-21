e118d0e03a748f4325e9eeb2a9215bea614dbb16
rohitarora15820 <rohitarora15820@gmail.com>
2021-07-21 11:12:14 +0530

    Updated Weather

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:climate/screens/location_screen.dart';
+import 'package:climate/services/weather.dart';
+
+
+
+
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+
+
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    WeatherModel weatherModel=WeatherModel();
+    var weatherData=await weatherModel.getLocationWeather();
+
+   Navigator.push(context, MaterialPageRoute(builder: (context)=> LocationScreen(locationWeather: weatherData,)));
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitFadingFour(
+          color: Colors.white,
+          size: 100.0,
+        )
+      ),
+    );
+  }
+}
